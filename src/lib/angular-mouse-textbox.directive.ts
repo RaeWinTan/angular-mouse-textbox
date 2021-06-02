39fd2430a0e59781a363a7d7d4f6989e9988c526
Tan Rae-Win <raewintan@gmail.com>
2021-06-02 17:03:54 +0800

    Display none when set to false

--- src/lib/angular-mouse-textbox.directive.ts
+++ src/lib/angular-mouse-textbox.directive.ts
@@ -17,28 +17,32 @@ export class AngularMouseTextboxDirective {
   @Input('mousetextbox') activate:boolean = false;
   @HostListener('document:mousemove',['$event'])
   mousemove(evt){
-    if(this.activate){
-      this.x = evt.pageX;
-      this.y = evt.pageY;
-      let div = this.renderer.createElement('div');
-      let text = this.renderer.createText(this.textIn);
-      this.renderer.appendChild(div, text);
-      this.renderer.appendChild(this.el.nativeElement, div);
-      this.renderer.addClass(div, "mousetextbox");
-      this.renderer.setStyle(div, "z-index",this.zindex);
-      this.renderer.setStyle(div, "left", this.x+"px");
-      this.renderer.setStyle(div, "top", this.y+"px");
-      if(this.oldChild !== undefined) {
-        this.renderer.removeChild(this.el.nativeElement,this.oldChild);
-        this.renderer.appendChild(div,text);
+
+    this.x = evt.pageX;
+    this.y = evt.pageY;
+    let div = this.renderer.createElement('div');
+    let text = this.renderer.createText(this.textIn);
+      if(this.activate) {
+        this.renderer.appendChild(div, text);
+        this.renderer.appendChild(this.el.nativeElement, div);
+        this.renderer.addClass(div, "mousetextbox");
+        this.renderer.setStyle(div, "z-index",this.zindex);
+        this.renderer.setStyle(div, "left", this.x+"px");
+        this.renderer.setStyle(div, "top", this.y+"px");
+        if(this.oldChild !== undefined) {
+          this.renderer.removeChild(this.el.nativeElement,this.oldChild);
+          this.renderer.appendChild(div,text);
+        }
+        this.oldChild = div;
+      }else{
+        if(div !== undefined) this.renderer.setStyle(div, "display", "none");
+        if(this.oldChild !== undefined) this.renderer.setStyle(this.oldChild, "display", "none");
       }
-      this.oldChild = div;
-    }
   }
 
 
   constructor(private el:ElementRef, private renderer: Renderer2 ) {
-  
+    console.log(el.nativeElement);
   }
 
 }
